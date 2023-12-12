f87851d536849fce6893a97745a2e956429e3c64
Daphnedouglas <134788606+Daphnedouglas@users.noreply.github.com>
2023-12-12 20:52:36 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,27 +1,40 @@
-# SYNTECH-Website
+# WebsiteforSyntech
 
-What tro include
-HOME
-follow design
+This template should help get you started developing with Vue 3 in Vite.
 
-EVENTS
-upcoming events (1 big banner for main event)
-ongoing events
-past event
+## Recommended IDE Setup
 
-WANT TO LEARN?
-Blog about tech
-Contact our mentors
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
 
-ABOUT US
-about syntech
-about level
-about huge
-about action
-org chart syntech
+## Type Support for `.vue` Imports in TS
 
-CONTACT US
-syntech room
-secretary's detail
-all ahod details
-Inquiry box
+TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin) to make the TypeScript language service aware of `.vue` types.
+
+If the standalone TypeScript plugin doesn't feel fast enough to you, Volar has also implemented a [Take Over Mode](https://github.com/johnsoncodehk/volar/discussions/471#discussioncomment-1361669) that is more performant. You can enable it by the following steps:
+
+1. Disable the built-in TypeScript Extension
+    1) Run `Extensions: Show Built-in Extensions` from VSCode's command palette
+    2) Find `TypeScript and JavaScript Language Features`, right click and select `Disable (Workspace)`
+2. Reload the VSCode window by running `Developer: Reload Window` from the command palette.
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/components/Footer.vue
@@ -0,0 +1,161 @@
+<template>
+  <footer class="footer" style="background-color: rgba(88, 188, 130, 0.1)">
+    <div class="custom">
+      <div class="custom-footer">
+        <div class="custom-footer-content">
+          <img src="" />
+          <div class="custom-icons">
+            <a href="#"><i class="bx bxl-instagram"></i></a>
+          </div>
+        </div>
+        <div class="custom-footer-content">
+          <h4>Associate with:</h4>
+          <img
+            src="../assets/icons/CIS_logo-removebg-preview.png"
+            height="110"
+            width="110"
+          />
+          <img
+            src="../assets/icons/csd-removebg-preview.png"
+            height="110"
+            width="110"
+          />
+        </div>
+
+        <div class="custom-footer-content">
+          <h4>Department :</h4>
+          <li><a href="#">Top</a></li>
+          <li><a href="#">Level</a></li>
+          <li><a href="#">Huge</a></li>
+          <li><a href="#">Action</a></li>
+        </div>
+
+        <div class="custom-footer-content">
+          <h4>Learn More</h4>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Event</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Want to Learn?</a></li>
+        </div>
+
+        <div class="custom-footer-content">
+          <h4>Contact Us</h4>
+          <li><a href="#">Secretary</a></li>
+          <li><a href="#">Ram</a></li>
+        </div>
+        <div class="custom-footer-content">
+          <h4>Social</h4>
+          <li><a href="#">Instagram</a></li>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <div
+    style="
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding-top: 10px;
+      padding-bottom: 10px;
+      background-color: rgba(88, 188, 130, 0.1);
+      color: black;
+    "
+  >
+    @2023 Syntech | All Rights Reserved
+  </div>
+</template>
+
+<style scoped>
+.custom {
+  padding: 0;
+  margin: 0;
+  text-decoration: none;
+  list-style: none;
+}
+
+.custom-footer {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(0px, auto));
+
+  position: relative;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.custom-footer-content h4 {
+  color: black; /*change back to white if want black footer */
+  margin-bottom: 1.5rem;
+  font-size: 20px;
+  font-weight: 550;
+}
+
+.custom-footer-content li {
+  margin-bottom: 16px;
+}
+
+.custom-footer-content li a {
+  display: block;
+  color: black;
+  font-size: 15px;
+  font-weight: 400;
+  transition: all.40s ease;
+}
+
+.custom-footer-content li a:hover {
+  transform: translateY(-3px) translateX(-5px);
+  color: black;
+}
+
+.custom-icons a {
+  display: inline-block;
+  font-size: 22px;
+  color: black;
+  margin-right: 17px;
+  transition: all 0.4s ease;
+}
+@keyframes tonext {
+  75% {
+    left: 0;
+  }
+  95% {
+    left: 100%;
+  }
+  98% {
+    left: 100%;
+  }
+  99% {
+    left: 0;
+  }
+}
+
+@keyframes tostart {
+  75% {
+    left: 0;
+  }
+  95% {
+    left: -300%;
+  }
+  98% {
+    left: -300%;
+  }
+  99% {
+    left: 0;
+  }
+}
+
+@keyframes snap {
+  96% {
+    scroll-snap-align: center;
+  }
+  97% {
+    scroll-snap-align: none;
+  }
+  99% {
+    scroll-snap-align: none;
+  }
+  100% {
+    scroll-snap-align: center;
+  }
+}
+</style>
